6c2ac848e0c3940011471c492867abb5f88475c9
kronsby <kronsby@gmail.com>
2023-01-11 22:50:26 -0500

    Added 3 more buttons and fixed some netcode

--- code.py
+++ code.py
@@ -2,8 +2,15 @@
 
 A MagTag program to trigger a Home Assistant webhook.
 
-This mainly connects to internet and waits for a button press. When a button
-press is received, it sends a POST request to a Home Assistant webhook.
+This mainly connects to internet and waits for a button press. Each of the 4
+buttons on the MagTag do something different.
+
+1. Plays Roar by Katy Perry
+2. Plays Think of What You've Done by Christina Vane
+3. Turns on the lights
+4. Turns off the lights
+
+If 1 or 2 are pressed while any song is playing it will stop.
 """
 import adafruit_requests
 import board
@@ -15,6 +22,11 @@ import time
 import wifi
 
 from secrets import secrets
+
+# Network setup
+pool = socketpool.SocketPool(wifi.radio)
+ssl_ctx = ssl.create_default_context()
+requests = adafruit_requests.Session(pool, ssl_ctx) 
 
 def check_connection():
     """Checks if we are connected to WiFi.
@@ -68,34 +80,61 @@ def send_post(url):
         True if 200 status code, otherwise False
     '''
     if not check_connection():
-        print('Lost connection. Reconnecting...')
+        print('Got disconnected... reconnecting')
         connect()
-    pool = socketpool.SocketPool(wifi.radio)
     print('Sending request to: %s' % url)
-    requests = adafruit_requests.Session(pool, ssl.create_default_context()) 
-    response = requests.post(url)
-    if response.status_code == 200:
-        return True
-    else:
-        print(response)
-        return False
+    try:
+        response = requests.post(url)
+        if response.status_code == 200:
+            return True
+        else:
+            print(response)
+    except Exception as e:
+        print(e)
+        print(wifi.radio.ap_info)
+    return False
 
 def main():
     set_image('/images/pleasewait.bmp')
     if connect():
         set_image('/images/roarbg.bmp')
-        btn = digitalio.DigitalInOut(board.BUTTON_A)
-        btn.direction = digitalio.Direction.INPUT
-        btn.pull = digitalio.Pull.UP
+        btn1 = digitalio.DigitalInOut(board.BUTTON_A)
+        btn1.direction = digitalio.Direction.INPUT
+        btn1.pull = digitalio.Pull.UP
+        btn2 = digitalio.DigitalInOut(board.BUTTON_B)
+        btn2.direction = digitalio.Direction.INPUT
+        btn2.pull = digitalio.Pull.UP
+        btn3 = digitalio.DigitalInOut(board.BUTTON_C)
+        btn3.direction = digitalio.Direction.INPUT
+        btn3.pull = digitalio.Pull.UP
+        btn4 = digitalio.DigitalInOut(board.BUTTON_D)
+        btn4.direction = digitalio.Direction.INPUT
+        btn4.pull = digitalio.Pull.UP
+        base_url = 'http://home.box:8123/api/webhook/'
         while True:
-            if not btn.value:
+            if not btn1.value:
                 # pressed
-                print('BTN pressed')
-                if send_post('http://home.box:8123/api/webhook/roar-trigger'):
+                print('Button 1 pressed')
+                if send_post(base_url + secrets['roar-trigger']):
                     print('Successfully sent POST request')
                     time.sleep(5)
                 else:
                     print('Error: Failed to send POST request') 
+            elif not btn2.value:
+                print('Button 2 pressed')
+                if send_post(base_url + secrets['think-of-what-you-done-trigger']):
+                    print('Successfully sent POST request')
+                    time.sleep(5)
+            elif not btn3.value:
+                print('Button 3 pressed')
+                if send_post(base_url + secrets['lights-on-trigger']):
+                    print('Successfully Sent POST request')
+                    time.sleep(5)
+            elif not btn4.value:
+                print('Button 4 pressed')
+                if send_post(base_url + secrets['lights-off-trigger']):
+                    print('Successfully Sent POST request')
+                    time.sleep(5)
             time.sleep(0.1)  # sleep for debounce
 
 main()
---
+++ copy-script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -rvu code.py /mnt/magtag/
+cp -rvu images /mnt/magtag/
+cp -rvu lib /mnt/magtag/
